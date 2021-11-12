1b45981dcbc6be0d3cc79d12c6240cf70a159fca
George7791 <93775628+George7791@users.noreply.github.com>
2021-11-12 13:04:42 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,31 @@
-let x = 0
 input.onButtonPressed(Button.A, function () {
-	
-})
-input.onGesture(Gesture.LogoUp, function () {
     x += -1
 })
-input.onGesture(Gesture.TiltLeft, function () {
-	
-})
 input.onButtonPressed(Button.B, function () {
-	
-})
-input.onGesture(Gesture.TiltRight, function () {
     x += 1
 })
-input.onGesture(Gesture.LogoDown, function () {
-    x += 1
-})
+let y = 0
+let random = 0
+let x = 0
+game.setScore(0)
+let difficulty = 1000
 basic.forever(function () {
     basic.clearScreen()
     led.plot(x, 4)
+    led.plot(random, y)
+    if (x == random && y == 4) {
+        led.stopAnimation()
+        game.gameOver()
+    }
 })
 basic.forever(function () {
-	
+    random = randint(0, 4)
+    y = 0
+    for (let index = 0; index < 4; index++) {
+        basic.pause(difficulty)
+        led.plot(randint(0, 4), y)
+        y += 1
+    }
+    difficulty += -10
+    game.addScore(1)
 })
